0b2268937f18031b52f475619445979ed998428b
tianbo <tianbo152@gmail.com>
2023-07-27 16:30:47 +0800

    angular

---
+++ pinduoduo/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = '拼多多';
+  tabs = ['热门', '男装', '手机'];
+}
